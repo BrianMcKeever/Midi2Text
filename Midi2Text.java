b818963c4e8d02cab88b8030d552abbcf3460bcb
Brian McKeever <keever@gmail.com>
2015-08-29 21:58:46 -0600

    we now output a lot more info

--- Midi2Text.java
+++ Midi2Text.java
@@ -99,29 +99,73 @@ public class Midi2Text implements ActionListener {
             String fileName = saveData();
             if(fileName == null) return;
             FileWriter textFile = new FileWriter(fileName);
-            textFile.write("Start Time" + "\t" + "Pitch" + "\t" +
-                    "Duration" + "\t" + "Dynamic" + "\n");
+            String title = s.getTitle();
+            textFile.write("#Title:\n");
+            textFile.write(title + "\n");
+
+            int timeSignatureNumerator = s.getNumerator();
+            int timeSignatureDenominator = s.getDenominator();
+            textFile.write("#Time Signature:\n");
+            textFile.write(timeSignatureNumerator + "/" + timeSignatureDenominator + "\n");
+
+            double bpm = s.getTempo();
+            textFile.write("#Beats per Minute:\n");
+            textFile.write(bpm + "\n");
+
+            int keyQuality = s.getKeyQuality();
+            textFile.write("#Key Quality:\n");
+            if(keyQuality == 1) {
+                textFile.write("Minor\n");
+            } else {
+                textFile.write("Major\n");
+            }
+
+            int keySignature = s.getKeySignature();
+            textFile.write("#Key Signature if positive number sharps otherwise flats:\n");
+            textFile.write(keySignature + "\n");
+
+            double longestRhythmValue = s.getLongestRhythmValue();
+            textFile.write("#Longest Rhythm Value:\n");
+            textFile.write(longestRhythmValue + "\n");
+
+            double shortestRhythmValue = s.getShortestRhythmValue();
+            textFile.write("#Shortest Rhythm Value:\n");
+            textFile.write(shortestRhythmValue + "\n");
+
+            textFile.write("#Start Time - Pitch - Duration - Dynamic - Pitch Name\n");
             // read note data and convert
             // get data values
+
             Enumeration enum1 = s.getPartList().elements();
             while (enum1.hasMoreElements()) {
                 Part part = (Part) enum1.nextElement();
+                textFile.write("#Part:\n");
+                textFile.write(part.getTitle() + "\n");
+                textFile.write("#Instrument:\n");
+                textFile.write(Integer.toString(part.getInstrument()) + "\n");
+                part.getTitle();
                 Enumeration enum2 = part.getPhraseList().elements();
                 while (enum2.hasMoreElements()) {
                     Phrase phrase = (Phrase) enum2.nextElement();
+                    textFile.write("#New Phrase:\n");
                     double startTime = phrase.getStartTime();
                     Enumeration enum3 = phrase.getNoteList().elements();
                     while (enum3.hasMoreElements()) {
                         Note note = (Note) enum3.nextElement();
-                        if (note.getPitch() != JMC.REST) {
-                            // start time
-                            textFile.write(Double.toString(startTime) + "\t");
-                            // pitch
-                            textFile.write(Integer.toString(note.getPitch()) + "\t");
-                            // duration
-                            textFile.write(Double.toString(note.getDuration()) + "\t");
-                            // velocity
-                            textFile.write(Integer.toString(note.getDynamic()) + "\n");
+                        // start time
+                        textFile.write(Double.toString(startTime) + "\t");
+                        // pitch
+                        textFile.write(Integer.toString(note.getPitch()) + "\t");
+                        // duration
+                        textFile.write(Double.toString(note.getDuration()) + "\t");
+                        // velocity
+                        textFile.write(Integer.toString(note.getDynamic()) + "\t");
+
+                        //string representation of note
+                        if(note.isRest()) {
+                            textFile.write("Rest\n");
+                        } else {
+                            textFile.write(note.getNote() + "\n");
                         }
                         startTime += note.getDuration();
                     }
